84fd90b40d8a593e2f4020cad1e9723274a5d657
yoshi-koyama <koyama@reytech.co.jp>
2023-11-18 15:51:08 +0900

    ポチではなくクロに犬の名前を変更

--- src/main/java/com/koyama/Main.java
+++ src/main/java/com/koyama/Main.java
@@ -2,7 +2,7 @@ package com.koyama;
 
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("ポチ", 3);
+        Dog dog = new Dog("クロ", 3);
         System.out.println(dog.getName());
         System.out.println(dog.getAge());
 
